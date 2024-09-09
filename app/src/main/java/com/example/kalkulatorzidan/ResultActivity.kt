61e70e174ad9151bf87d3fc3fc8e07e50ece5b87
mzidan25 <muhammadzidan25@student.ub.ac.id>
2024-09-09 10:14:13 +0700

    Initial Commit

--- app/src/main/java/com/example/kalkulatorzidan/ResultActivity.kt
+++ app/src/main/java/com/example/kalkulatorzidan/ResultActivity.kt
@@ -7,17 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 class ResultActivity : AppCompatActivity() {
 
     private lateinit var textResult: TextView
+    private lateinit var textNameNim: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         textResult = findViewById(R.id.textResult)
+        textNameNim = findViewById(R.id.textNameNim)
 
         val result = intent.getDoubleExtra("result", 0.0)
         val nim = intent.getStringExtra("nim")
         val nama = intent.getStringExtra("nama")
 
-        textResult.text = "Hasil: $result\nNIM: $nim\nNama: $nama"
+        textResult.text = "Hasil: $result"
+
+        textNameNim.text = "$nim - $nama"
     }
 }
+
